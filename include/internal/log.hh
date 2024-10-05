b62f6fb8197e28d854d5122ce437a165a8abd66e
aenlemmea <118059484+aenlemmea@users.noreply.github.com>
2024-10-05 16:31:21 +0000

    Added a micro logger for logging

---
+++ include/internal/log.hh
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef LOG_H // Header guards as ran into some portability issues.
+#define LOG_H
+
+#include <iostream>
+#include <source_location>
+enum class LOG_LEVEL : char {
+    INFO = 'I',
+    WARNING = 'W',
+    ERROR = 'E',
+};
+
+class Logger {
+  public:
+#if defined(DEBUG)
+
+    static void
+    log(LOG_LEVEL const level, const std::string_view str,
+        const std::source_location src = std::source_location::current()) {
+        std::cout << static_cast<char>(level) << " | " << src.file_name()
+                  << ":" << src.function_name() << ":" << src.line() << " | "
+                  << str << "\n";
+    }
+#else
+    static void
+    log(LOG_LEVEL const level, const std::string_view str,
+        const std::source_location src = std::source_location::current()) {}
+#endif
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include "internal/log.hh"
+
 auto main(int argc, char** argv) -> int {
+  Logger::log(LOG_LEVEL::INFO, "Hi from main!");
 	return 0;
 }
